eae9acee0564aae6a7118cc8c074b8c0d72fd8d5
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-01-11 03:32:02 +0500

    add info and tech sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,16 +13,10 @@ let homeList = document.querySelector(".home__list")
 let firstItem = document.querySelectorAll(".home__item")[0]
 let arrowBtn = document.querySelectorAll(".home__arrow")
 let scrollWidth = homeList.scrollWidth - homeList.clientWidth
-const showHideBtn = () => {
-  arrowBtn[0].style.display = homeList.scrollLeft == 0 ? "none" : "block"
-  arrowBtn[1].style.display = homeList.scrollLeft == scrollWidth ? "none" : "block"
-}
-showHideBtn()
 arrowBtn.forEach(button => {
   button.addEventListener("click", () => {
     let firstItemWidth = firstItem.clientWidth;
     homeList.scrollLeft += button.id == "left" ? -firstItemWidth : firstItemWidth
-    setTimeout(() => showHideBtn(), 60)
   })
 });
 
@@ -31,11 +25,32 @@ arrowBtn.forEach(button => {
 let buildList = document.querySelector(".build__list")
 let buildItem = document.querySelectorAll(".build__item")[0]
 let buildBtn = document.querySelectorAll(".build__card-button")
-let buildScrollWidth = buildList.scrollWidth - buildList.clientWidth
 
 buildBtn.forEach(button => {
   button.addEventListener("click", () => {
     let firstBuildItem = buildItem.clientWidth
     buildList.scrollLeft += button.id == "build-left" ? -firstBuildItem : firstBuildItem
+  })
+})
+
+////////////////////////////////////////////
+
+let techDesc = document.querySelector(".technologies__description")
+let techBtn = document.querySelector(".technologies__toggle")
+
+techBtn.addEventListener("click", () => {
+  techDesc.classList.toggle("showTech")
+})
+
+////////////////////////////////////////////
+
+let infoList = document.querySelector(".information__list")
+let infoItem = document.querySelectorAll(".information__item")[0]
+let infoBtn = document.querySelectorAll(".information__button")
+
+infoBtn.forEach(button => {
+  button.addEventListener("click", () => {
+    let firstInfoItem = infoItem.clientWidth
+    infoList.scrollLeft += button.id == "info-left" ? -firstInfoItem : firstInfoItem
   })
 })
